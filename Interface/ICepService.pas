16998aa1de2d46033a91f284e3db6fcfa5f94ddb
Giovani Luiz de Santi <giovaniluiz_santi@live.com>
2024-09-23 19:19:43 -0300

    Add files via upload

---
+++ Interface/ICepService.pas
@@ -0,0 +1,14 @@
+unit ICepService;
+
+interface
+
+type
+  ICepServices = interface
+    ['{A4C8B877-BA88-4EE6-B676-F63D8274CF48}']  // GUID único para a interface
+    function ConsultarCepPorCep(const ACep: string): string;
+    function ConsultarCepPorEndereco(const AEstado, ACidade, ALogradouro: string): string;
+  end;
+
+implementation
+
+end.
